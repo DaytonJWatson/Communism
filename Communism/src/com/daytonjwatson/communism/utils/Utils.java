047942b746b430299f2c79980b5ee0381582e51c
DaytonJWatson <45436514+DaytonJWatson@users.noreply.github.com>
2025-12-04 15:11:33 -0700

    Enhance chat messaging and propaganda

--- Communism/src/com/daytonjwatson/communism/utils/Utils.java
+++ Communism/src/com/daytonjwatson/communism/utils/Utils.java
@@ -3,7 +3,15 @@ package com.daytonjwatson.communism.utils;
 import org.bukkit.ChatColor;
 
 public class Utils {
-	public static String color(String string) {
-		return ChatColor.translateAlternateColorCodes('&', string);
-	}
+
+        public static final String PREFIX = ChatColor.DARK_RED + "" + ChatColor.BOLD + "☭ "
+                        + ChatColor.RED + "Communism " + ChatColor.DARK_GRAY + "» " + ChatColor.GRAY;
+
+        public static String color(String string) {
+                return ChatColor.translateAlternateColorCodes('&', string);
+        }
+
+        public static String prefix(String message) {
+                return PREFIX + ChatColor.translateAlternateColorCodes('&', message);
+        }
 }
